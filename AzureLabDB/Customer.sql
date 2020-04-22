51ca42e34a22a740e6ece174f4b48264099b210c
Mike Helstrom <mike_helstrom@hotmail.com>
2020-04-22 08:28:51 -0400

    Added test column

--- AzureLabDB/Customer.sql
+++ AzureLabDB/Customer.sql
@@ -3,5 +3,6 @@
 	[CustomerID] INT NOT NULL, 
     [CustomerName] VARCHAR(50) NULL, 
     [CustomerLocation] NCHAR(10) NULL, 
+    [TestColumn] VARCHAR(2) NULL
     CONSTRAINT [PK_Table1] PRIMARY KEY ([CustomerID]) 
 )
